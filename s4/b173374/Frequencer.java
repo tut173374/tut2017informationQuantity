442c032f95a9efd0afdd87c26d2d8971c20ac441
Watanabe Yuya <y173374@im143.mac.network>
2018-01-09 18:06:48 +0900

    Frequencer update

--- s4/b173374/Frequencer.java
+++ s4/b173374/Frequencer.java
@@ -19,7 +19,127 @@ public class Frequencer implements FrequencerInterface{
     // Code to Test, *warning: This code  contains intentional problem*
     byte [] myTarget;
     byte [] mySpace;
-    public void setTarget(byte [] target) { myTarget = target;}
+    boolean targetReady	= false;
+    boolean spaceReady	= false;
+    int	[] suffixArray;
+
+    
+    private void printSuffixArray() {
+	if(spaceReady) {
+	    for(int i=0; i< mySpace.length; i++){
+		int s = suffixArray[i];
+		for(int	j=s;j<mySpace.length;j++){
+		    System.out.write(mySpace[j]);
+		}
+		System.out.write('\n');
+	    }
+	}
+    }
+
+    private int suffixCompare(int i, int j){
+	String suffix_i, suffix_j;
+	byte[] letter = new byte [10];
+	for(int k=i;k<mySpace.length;k++){
+	    letter[k-i] = mySpace[k];
+	}
+	suffix_i = new String (letter);
+	for(int k=j;k<mySpace.length;k++){
+	    letter[k-j] = mySpace[k];
+	}
+	suffix_j = new String (letter);
+	
+	int compare = suffix_i.compareTo(suffix_j);
+
+	if (compare >= 1) compare = 1;
+	else compare = -1;
+	
+	return compare;
+    }
+
+    public void setSpace(byte []space) {
+	mySpace = space; if (mySpace.length>0) spaceReady = true;
+	suffixArray = new int[space.length];
+	for(int i = 0; i < space.length; i++){
+	    suffixArray[i] = i;
+	}
+	printSuffixArray();
+	//sort
+	int count = 0;
+        for (int i = 0; i < suffixArray.length - 1; i++) {
+            for (int j = suffixArray.length - 1; j > i; j--) {
+		if(suffixCompare(j-1,j) == 1) {
+                    int tmpNum = suffixArray[j - 1];
+                    suffixArray[j - 1] = suffixArray[j];
+                    suffixArray[j] = tmpNum;
+                    count++;
+                }
+            }
+        }
+	printSuffixArray();
+    }
+
+    private int targetCompare(int i, int j, int end){
+	return 0;
+    }
+
+    private int subByteStartIndex(int start, int end){
+	return suffixArray.length;
+    }
+
+    private int subByteEndIndex(int start, int end){
+	return suffixArray.length;
+    }
+
+    public int subByteFrequency(int start, int end){
+	int spaceLength = mySpace.length;
+	int count = 0;
+	for(int offset = 0; offset < spaceLength - (end - start); offset++) {
+	    boolean abort = false;
+	    for(int i = 0; i < (end - start); i++) {
+		if(myTarget[start+i] != mySpace[offset+i]) { abort = true; break;}
+	    }
+	    if(abort == false) {count++;}
+	}
+	int first = subByteStartIndex(start,end);
+	int last1 = subByteEndIndex(start,end);
+	
+	//debug
+	for(int k = start; k < end; k++) {System.out.write(myTarget[k]);}
+	System.out.printf(" : first = %d last1 = %d \n", first, last1);
+	
+	return last1-first;
+    }
+
+    public void setTarget(byte [] target) {
+	myTarget = target; if(myTarget.length>0) targetReady = true;
+    }
+
+    public int frequency() {
+	if(targetReady == false) return -1;
+	if(spaceReady == false) return 0;
+	return subByteFrequency(0, myTarget.length);
+    }
+
+    public static void main(String[] args){
+	Frequencer frequencerObject;
+	try {
+	    frequencerObject = new Frequencer();
+	    frequencerObject.setSpace("Hi Ho Hi Ho".getBytes());
+	    /*frequencerObject.setTarget("H".getBytes());
+	    int result = frequencerObject.frequency();
+	    System.out.print("Freq = "+result+" ");
+	    if(4 == result) {System.out.println("OK");}
+	    else {System.out.println("WRONG");}*/
+	}
+	catch(Exception e){
+	    System.out.println("STOP");
+	}
+    }
+}
+
+
+    
+    /*public void setTarget(byte [] target) { myTarget = target;}
     public void setSpace(byte []space) { mySpace = space; }
     public int frequency() {
 	int targetLength = myTarget.length;
@@ -57,6 +177,6 @@ public class Frequencer implements FrequencerInterface{
 	catch(Exception e) {
 	    System.out.println("Exception occurred: STOP");
 	}
-    }
-}	    
+    }}	*/
+    
 	    
